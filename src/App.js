e402c02f84c6933b92baf8bded06860c52684e51
Sanjay <sanjaydutta976.com>
2023-07-21 20:00:23 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Todo from "./components/todo";
+// import Temp from "./components/weather/temp";
+
+const App = () => {
+  return (
+    <>
+      <Todo />
+      {/* <Temp /> */}
+    </>
+  );
+};
+
+export default App;
